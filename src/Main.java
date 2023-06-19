3151b30be2d0901e5ecd2705137aa6ea36b5e0f8
Andrei <andreisita1998@yahoo.com>
2023-06-19 11:57:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double PI = 3.14;
+        double R=5;
+        double H = 50;
+        double V = (R*R) * PI * H;
+        System.out.println(V);
+    }
+}
